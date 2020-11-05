261de60c4e6afe9437ba209318bc83a549df17d3
hunter <hunter0697@gmail.com>
2020-11-05 11:57:24 +0800

    20201105homework2:Table Selection and Count

---
+++ TableTest/TableViewController.swift
@@ -0,0 +1,123 @@
+//
+//  TableViewController.swift
+//  TableTest
+//
+//  Created by NDHU_CSIE on 2020/11/5.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    @IBOutlet var myLabel:UILabel!
+    @IBOutlet var totalLabel:UILabel!
+
+    
+    var items = ["watermellon", "apple", "orange", "strawberry", "pear"]
+    var prices = [500, 100, 90, 200, 150]
+    var itemIsSelect = Array(repeating: false, count: 21)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return items.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "datacell", for: indexPath) as! TableViewCell
+
+        cell.name.text = items[indexPath.row]
+        cell.price.text = "\(prices[indexPath.row])"
+        
+
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        self.itemIsSelect[indexPath.row] = itemIsSelect[indexPath.row] ? false : true //left is ture or false
+        cell?.accessoryType = itemIsSelect[indexPath.row] ?.checkmark : .none
+        var total = 0
+        //var j = 0
+        for (i, j) in zip(itemIsSelect, 0...5){
+            
+            if i == true {
+                total += prices[j]
+            }
+            //j += 1
+        }
+        totalLabel.text = "Total price is \(total)"
+        
+         // Deselect a row
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+
+    }
+    
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
